87be67d465b71657e798cda749eeb03d09a505a2
Monalisha <118917412+monalisha-jena@users.noreply.github.com>
2024-08-28 18:20:05 +0530

    Add files via upload

---
+++ controllers/user.js
@@ -0,0 +1,50 @@
+//ROUTES
+
+const User = require('../models/user');
+
+async function handleGetAllUsers(req, res){
+    const allDbUsers = await User.find({});
+    return res.json(allDbUsers);
+}
+
+async function handleGetAllUserById(req, res){
+    const user = await User.findById(req.params.id); 
+    if(!user){
+        return res.status(404).json({error: "user not found"}); 
+    }
+    return res.json(user);
+}
+
+async function handleUpdateUserById(req, res){
+    await User.findByIdAndUpdate(req.params.id, {lastName: "Jena"});
+    return res.json({status: "success"});
+}
+
+async function handleDeleteUserById(req, res){
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({status: "success"});
+}
+
+async function handleCreateNewUser(req, res){
+    const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.city){
+        return res.status(400).json({msg: "All fields are required"}); 
+    }
+    const result =  await User.create({
+        firstName: body.first_name, 
+        lastName: body.last_name, 
+        email: body.email, 
+        gender: body.gender, 
+        city: body.city,
+    })
+
+    return res.status(201).json({msg: "success", id: result._id});
+}
+
+module.exports = {
+    handleGetAllUsers, 
+    handleGetAllUserById, 
+    handleUpdateUserById, 
+    handleDeleteUserById,
+    handleCreateNewUser,
+};
